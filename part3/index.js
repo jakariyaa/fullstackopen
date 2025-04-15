b2ccae9db676144909b52cb56fec330323b6aa6e
jakariyaa <jakariya3460@gmail.com>
2025-04-16 01:10:39 +0600

    add 3.8

--- part3/index.js
+++ part3/index.js
@@ -4,7 +4,11 @@ const app = express()
 
 app.use(express.json())
 
-app.use(morgan('tiny'))
+morgan.token('req-body', (request) => {
+  return request.body ? JSON.stringify(request.body) : ''
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
 
 let persons = [
   {
