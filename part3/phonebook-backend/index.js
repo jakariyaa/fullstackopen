4df9336749422f73182b9f81e77d86595e703a78
jakariyaa <jakariya3460@gmail.com>
2025-04-15 23:26:21 +0600

    add 3.3

---
+++ part3/phonebook-backend/index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+  {
+    "id": "1",
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": "2",
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": "3",
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": "4",
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  const now = new Date().toString()
+  response.send(
+    `<div>Phonebook has info for ${persons.length} people</div>` +
+    `</br>` +
+    `<div>${now}</div>`
+  )
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(p => p.id === id)
+  person ? response.json(person) : response.status(404).end()
+})
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log("Server running at:", PORT)
+})
