804c73d5dcac6fa16b40fcf12f42d6453fe5ee9e
jakariyaa <jakariya3460@gmail.com>
2025-04-27 22:45:34 +0600

    5.2

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -10,6 +10,12 @@ const App = () => {
   const [user, setUser] = useState(null)
 
   useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedInUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+    console.log(user)
     blogService.getAll().then(blogs =>
       setBlogs(blogs)
     )
@@ -22,13 +28,24 @@ const App = () => {
       const user = await loginService.login({
         username, password
       })
+
+      window.localStorage.setItem(
+        'loggedInUser', JSON.stringify(user)
+      )
+
       setUser(user)
-      console.log(user);
       setUsername('')
       setPassword('')
     } catch (error) {
       console.log(error);
     }
+  }
+
+  const handleLogout = (event) => {
+    event.preventDefault()
+
+    window.localStorage.removeItem('loggedInUser')
+    setUser(null)
   }
 
   if (!user) {
@@ -53,7 +70,9 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} is logged in</p>
+      <div>{user.name} is logged in
+        <button onClick={handleLogout}>Log Out</button>
+      </div>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
